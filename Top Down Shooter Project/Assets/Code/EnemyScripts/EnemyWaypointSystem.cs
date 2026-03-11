03f6aabfde98e8212e347a267278f4ab9894a72e
Voxin <mark@digitalice.nl>
2026-03-11 13:34:12 +0100

    Add EnemyWaypointSystem with player chase
    Introduce a refactored EnemyWaypointSystem script (moved into Assets/Code/EnemyScripts) that patrols waypoints and switches to chasing the player when within a configurable distance. The new script adds serialized fields for waypoints, player reference, chase distance and enemy speed, and rotates the enemy to face movement direction. The old simple waypoint script was removed and folder meta/rename updates were added. Also include related Unity project updates: BackupScene.unity changes, package/manifest updates, build/navmesh/project settings and TagManager modifications.

---
+++ Top Down Shooter Project/Assets/Code/EnemyScripts/EnemyWaypointSystem.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class EnemyWaypointSystem : MonoBehaviour
+{
+    // Waypoints
+    [SerializeField] Vector3[] waypoints;
+    [SerializeField] int waypointIndex = 0;
+
+    // Player
+    [SerializeField] private Transform player;
+    [SerializeField] private float DisToMove = 5;
+
+    // Enemy Stats
+    [SerializeField] private float enemySpeed = 2f;
+
+    private bool isChasing = false;
+
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        // Check of enemy moet chasen of waypoints
+        if (distance < DisToMove)
+            isChasing = true;
+        else
+            isChasing = false;
+
+        if (isChasing)
+            ChasePlayer();
+        else
+            Move();
+    }
+
+    // waypoints
+    private void Move()
+    {
+        if (waypointIndex < waypoints.Length)
+        {
+            Vector3 direction = (waypoints[waypointIndex] - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[waypointIndex], enemySpeed * Time.deltaTime);
+
+            if (transform.position == waypoints[waypointIndex])
+                waypointIndex++;
+        }
+        else
+        {
+            waypointIndex = 0;
+        }
+    }
+
+    //chase player
+    private void ChasePlayer()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        transform.position = Vector3.MoveTowards(transform.position, player.position, enemySpeed * Time.deltaTime);
+    }
+}
