6614e82b43c645df3fdff73f1f64b3ba48b0c666
BlueFireKnightYT <vanmeerveldsGames@outlook.com>
2026-03-12 14:11:04 +0100

    bullet trail

--- Top Down Shooter Project/Assets/Code/PlayerShooting.cs
+++ Top Down Shooter Project/Assets/Code/PlayerShooting.cs
@@ -8,9 +8,19 @@ public class PlayerShooting : MonoBehaviour
     public GameObject gunFlash;
     public Transform shootPoint;
 
+    public float bulletTravelLength;
     public float shootCooldown;
 
+    public LayerMask excludedLayer;
     bool isShooting = false;
+
+    public TrailRenderer trailPrefab;
+    public float bulletSpeed = 100f;
+
+    private void Update()
+    {
+        Debug.DrawRay(shootPoint.position, shootPoint.up * bulletTravelLength);
+    }
 
     public void ShootInput(InputAction.CallbackContext context)
     {
@@ -22,7 +32,7 @@ public class PlayerShooting : MonoBehaviour
         if (context.canceled)
         {
             isShooting = false;
-            StopAllCoroutines();
+            StopCoroutine(Shoot());
             muzzleFlashLight.SetActive(false);
             gunFlash.SetActive(false);
         }
@@ -32,17 +42,46 @@ public class PlayerShooting : MonoBehaviour
     {
         while (isShooting)
         {
-            //raycast hier
+            Vector3 targetPoint;
+            RaycastHit2D hit = Physics2D.Raycast(shootPoint.position, shootPoint.up, bulletTravelLength, ~excludedLayer);
 
-            //Muzzle flash
+            if (hit.collider != null)
+            {
+                targetPoint = hit.point;
+                Debug.Log(hit.collider.gameObject.name);
+            }
+            else
+            {
+                targetPoint = shootPoint.position + (shootPoint.up * bulletTravelLength);
+            }
+
+            TrailRenderer trail = Instantiate(trailPrefab, shootPoint.position, Quaternion.identity);
+            StartCoroutine(SpawnTrail(trail, targetPoint));
+
             muzzleFlashLight.SetActive(true);
             gunFlash.SetActive(true);
             yield return new WaitForSeconds(.1f);
             muzzleFlashLight.SetActive(false);
             gunFlash.SetActive(false);
 
-            //wachten voor nieuwe kogel
             yield return new WaitForSeconds(shootCooldown);
         }
+    }
+
+    private IEnumerator SpawnTrail(TrailRenderer trail, Vector3 hitPoint)
+    {
+        float time = 0;
+        Vector3 startPosition = trail.transform.position;
+
+        while (time < 1)
+        {
+            trail.transform.position = Vector3.Lerp(startPosition, hitPoint, time);
+            time += Time.deltaTime * bulletSpeed;
+
+            yield return null;
+        }
+
+        trail.transform.position = hitPoint;
+        Destroy(trail.gameObject, trail.time);
     }
 }
