3c85809956fb7cfd229692dd90b9ff6c68078ecf
BlueFireKnightYT <vanmeerveldsGames@outlook.com>
2026-03-13 14:17:41 +0100

    Improvements

--- Top Down Shooter Project/Assets/Code/CamMoveScript.cs
+++ Top Down Shooter Project/Assets/Code/CamMoveScript.cs
@@ -2,11 +2,27 @@ using UnityEngine;
 
 public class CamMoveScript : MonoBehaviour
 {
-    public Vector3[] cameraPositions;
-    public int targetPos = 0;
 
-    private void FixedUpdate()
+    //public Vector3[] cameraPositions;
+    //public int targetPos = 0;
+
+    //private void FixedUpdate()
+    //{
+    //    transform.position = Vector3.Lerp(transform.position, cameraPositions[targetPos], 5 * Time.deltaTime);
+    //}
+    GameObject player;
+    float playerPosY;
+    public float lerpSpeed;
+
+    private void Start()
     {
-        transform.position = Vector3.Lerp(transform.position, cameraPositions[targetPos], 5 * Time.deltaTime);
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void Update()
+    {
+        playerPosY = player.transform.position.y;
+        Vector3 nextCamPos = new Vector3(0, playerPosY, -10);
+
+        transform.position = Vector3.Lerp(transform.position, nextCamPos, lerpSpeed * Time.deltaTime);
     }
 }
--- Top Down Shooter Project/Assets/Code/CamTrigger.cs
+++ Top Down Shooter Project/Assets/Code/CamTrigger.cs
@@ -16,18 +16,18 @@ public class CamTrigger : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
-        {
-            if (camMoveScript.targetPos == TriggerNumber - 1)
-            {
-                camMoveScript.targetPos = TriggerNumber;
-                player.transform.position = new Vector2(player.transform.position.x, player.transform.position.y + 1);
-            }
-            else
-            {
-                camMoveScript.targetPos = TriggerNumber - 1;
-                player.transform.localPosition = new Vector2(player.transform.position.x, player.transform.position.y - 1);
-            }
-        }
+        //if (collision.CompareTag("Player"))
+        //{
+        //    if (camMoveScript.targetPos == TriggerNumber - 1)
+        //    {
+        //        camMoveScript.targetPos = TriggerNumber;
+        //        player.transform.position = new Vector2(player.transform.position.x, player.transform.position.y + 1);
+        //    }
+        //    else
+        //    {
+        //        camMoveScript.targetPos = TriggerNumber - 1;
+        //        player.transform.localPosition = new Vector2(player.transform.position.x, player.transform.position.y - 1);
+        //    }
+        //}
     }
 }
